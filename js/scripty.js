5ee555c7e010066e28bc10a69a558f78fad6db5f
Ramon Silva Freitas <ramonsilvaf@gmail.com>
2023-05-22 17:27:51 -0300

    criação de pastas e documentos 1 etapa

---
+++ js/scripty.js
@@ -0,0 +1,12 @@
+function calculocirculo1(){
+    let raio = parseFloat(document.getElementById("Raio: ").value)
+    let area = (pi * raio *(raio,2)
+    alert("Área: " + area)
+}
+
+function calculocirculo2(){
+    const PI = 3.14
+    let raio = prompt("Raio: ")
+    let area = 3.14 * raio * raio
+    alert("Área: " + area)
+}
